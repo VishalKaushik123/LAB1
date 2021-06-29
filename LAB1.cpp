282f979d9436d95109cd99f13a17389fda0945e4
VishalKaushik123 <84299956+VishalKaushik123@users.noreply.github.com>
2021-06-29 18:22:18 -0500

    Add files via upload

---
+++ LAB1.cpp
@@ -0,0 +1,32 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/Vishal/Documents/CTDProjects/LAB1/src/LAB1.ino"
+/*
+ * Project LAB1
+ * Description:
+ * Author:
+ * Date:
+ */
+void setup();
+void loop();
+#line 7 "c:/Users/Vishal/Documents/CTDProjects/LAB1/src/LAB1.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  pinMode(D5, OUTPUT);
+}
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  digitalWrite(D5, HIGH);
+  delay(1000);
+  digitalWrite(D5, LOW);
+  delay(1000);
+
+
+}
